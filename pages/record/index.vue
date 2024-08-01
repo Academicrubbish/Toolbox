48d70f32b7df0b55ae30e6d964cf9e25b7c8619b
yuanchuang <1226377893@qq.com>
2024-08-01 18:01:03 +0800

    完善提交记录表单

--- pages/record/index.vue
+++ pages/record/index.vue
@@ -1,55 +1,55 @@
+<!--
+ * @Author: yuanchuang 1226377893@qq.com
+ * @Date: 2024-08-01 17:31:22
+ * @LastEditors: yuanchuang 1226377893@qq.com
+ * @LastEditTime: 2024-08-01 17:55:13
+ * @FilePath: \Toolbox\pages\record\index.vue
+ * @Description: 
+ * 
+ * Copyright (c) 2024 by 坤智数联科技(宁波), All Rights Reserved. 
+-->
 <template>
   <view>
-    <cu-custom bgColor="bg-gradual-pink" :isBack="true">
+    <cu-custom bgcolor="bg-gradual-pink" :isBack="true">
       <block slot="backText">返回</block>
       <block slot="content">表单</block>
     </cu-custom>
-    <uni-forms
-      ref="form"
-      :modelValue="formData"
-      :rules="rules"
-      :label-width="77"
-    >
-      <uni-forms-item name="recordType" label="记录类型" required>
-        <easy-select
-          ref="easySelect"
-          size="small"
-          :value="recordTypeLabel"
-          @selectOne="selectRecordType"
-          :options="recordTypeOptions"
-        />
-      </uni-forms-item>
-      <uni-forms-item name="title" label="标题" required>
-        <uni-easyinput
-          v-model="formData.title"
-          :maxlength="20"
-          placeholder="请填写事件标题"
-        />
-      </uni-forms-item>
+    <view style="padding: 0 20rpx;">
+      <uni-forms ref="form" :modelValue="formData" :rules="rules" :label-width="78">
+        <uni-forms-item name="recordType" label="记录类型" required>
+          <easy-select
+            ref="easySelect"
+            size="small"
+            :value="recordTypeLabel"
+            @selectOne="selectRecordType"
+            :options="recordTypeOptions"
+          />
+        </uni-forms-item>
+        <uni-forms-item name="title" label="标题" required>
+          <uni-easyinput v-model="formData.title" :maxlength="20" placeholder="请填写事件标题" />
+        </uni-forms-item>
 
-      <uni-forms-item name="summary" label="事件简介" required>
-        <uni-easyinput
-          type="textarea"
-          v-model="formData.summary"
-          placeholder="请输入简介"
-        />
-      </uni-forms-item>
+        <uni-forms-item name="summary" label="事件简介" required>
+          <uni-easyinput type="textarea" v-model="formData.summary" placeholder="请输入简介" />
+        </uni-forms-item>
 
-      <uni-forms-item name="completionType" label="完成方式" required>
-        <uni-data-checkbox
-          v-model="formData.completionType"
-          :localdata="completionTypeOptions"
-        />
-      </uni-forms-item>
+        <uni-forms-item name="completionType" label="完成方式" required>
+          <uni-data-checkbox v-model="formData.completionType" :localdata="completionTypeOptions" />
+        </uni-forms-item>
 
-      <uni-forms-item name="timeSpent" label="耗时(min)" required>
-        <uni-easyinput
-          v-model="formData.timeSpent"
-          :maxlength="20"
-          placeholder="耗时"
-        />
-      </uni-forms-item>
-    </uni-forms>
+        <uni-forms-item name="completionPeriodStart" label="开始时间" required>
+          <uni-datetime-picker type="datetime" v-model="formData.completionPeriodStart" />
+        </uni-forms-item>
+
+        <uni-forms-item name="completionPeriodEnd" label="结束时间" required>
+          <uni-datetime-picker type="datetime" v-model="formData.completionPeriodEnd" />
+        </uni-forms-item>
+
+        <uni-forms-item name="timeSpent" label="耗时(min)" required>
+          <uni-easyinput v-model="formData.timeSpent" :maxlength="20" placeholder="耗时" />
+        </uni-forms-item>
+      </uni-forms>
+    </view>
   </view>
 </template>
 
@@ -57,7 +57,7 @@
 import easySelect from "@/component/easy-select/easy-select.vue";
 export default {
   components: {
-    "easy-select": easySelect,
+    "easy-select": easySelect
   },
   data() {
     return {
@@ -66,18 +66,18 @@ export default {
       recordTypeOptions: [
         { label: "学习", value: 0 },
         { label: "工作", value: 1 },
-        { label: "其他", value: 2 },
+        { label: "其他", value: 2 }
       ],
       // 单选数据源
       completionTypeOptions: [
         {
           text: "碎片完成",
-          value: 0,
+          value: 0
         },
         {
           text: "整段完成",
-          value: 1,
-        },
+          value: 1
+        }
       ],
       formData: {
         recordType: 0,
@@ -85,9 +85,11 @@ export default {
         summary: null,
         completionType: null,
         completionPeriod: null,
+        completionPeriodStart: null,
+        completionPeriodEnd: null,
         completionTime: null,
-        timeSpent: null,
-      },
+        timeSpent: null
+      }
     };
   },
   methods: {
@@ -103,13 +105,14 @@ export default {
     },
     recordTypePickerChange(e) {
       this.form.recordType = e.detail.value;
-    },
-  },
+    }
+  }
 };
 </script>
 
-<style>
-.cu-form-group .title {
-  min-width: calc(4em + 15px);
+<style scoped>
+.uni-forms-item__content {
+  display: flex !important;
+  align-items: center !important;
 }
 </style>
